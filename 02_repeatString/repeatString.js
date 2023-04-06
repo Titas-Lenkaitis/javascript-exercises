ee862d03dc84d97cd2af70b12f7bb6f26ec7ef2e
Titas Lenkaitis <titas.lenkaitis1@gmail.com>
2023-04-06 18:48:19 +0100

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num == 0) {
+        return string = "";
+    } else if (num < 0) {
+        return string = "ERROR";
+    }
+    string2 = string;
+    for (i = 1; i < num; i++) {
+        string += string2;
+    }
+    return string;
 };
 
 // Do not edit below this line
