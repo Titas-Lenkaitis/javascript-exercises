2645400f62cd887e618e493ce78b12c2e2aa4960
Titas-Lenkaitis <titas.lenkaitis1@gmail.com>
2023-04-08 16:37:43 +0100

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+  result = array.filter(x => !args.includes(x));
+  return result;
 };
 
 // Do not edit below this line
