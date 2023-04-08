9cb4f6fbfbab3753209519842c789f1ce89cbc47
Titas-Lenkaitis <titas.lenkaitis1@gmail.com>
2023-04-08 17:40:42 +0100

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  num = 0;
+  if (num1 < 0 || num2 < 0 || !(typeof num1 == "number") || !(typeof num2 == "number")) {
+    return num = "ERROR";
+  } else if (num2 < num1) {
+      while (num2 <= num1) {
+        num += num2;
+        num2++;
+    }
+  } else {
+      while (num1 <= num2) {
+        num += num1;
+        num1++;
+  }}
+  return num;
 };
 
 // Do not edit below this line
