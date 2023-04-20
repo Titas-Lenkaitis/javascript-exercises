cef36934c9d36b85a4cc585467dca22d7f80a52c
Titas-Lenkaitis <titas.lenkaitis1@gmail.com>
2023-04-20 19:29:54 +0100

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  total = 0;
+	for (i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  num = 1;
+  for (i = 0; i < array.length; i++) {
+    num *= array[i];
+  }
+  return num;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	num = 1;
+  for (i = 1; i <= a; i++) {
+    num *= i;
+  }
+  return num;
 };
 
 // Do not edit below this line
