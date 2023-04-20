afe14936e88883ebf4c6a83cf399d0786384073d
Titas-Lenkaitis <titas.lenkaitis1@gmail.com>
2023-04-20 20:26:27 +0100

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (word) {
+  word = word.toLowerCase().replace(/[!"£$%^&*()_+-=,;'#/:@~ ]/g, "");
+  reverseWord = word.split("").reverse().toString().replace(/,/g, "");
+  return (reverseWord == word)
+  }
+palindromes("A car, a man, a maraca.")
 // Do not edit below this line
 module.exports = palindromes;
