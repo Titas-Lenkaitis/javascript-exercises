3824aaba2d63dd8daed5228698ed17c52794dfdd
Titas-Lenkaitis <titas.lenkaitis1@gmail.com>
2023-04-20 21:10:05 +0100

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+  number = parseInt(number);
+  if (number < 0) {
+    return "OOPS";
+  }
+  num1 = 1;
+  num2 = 0;
+  num3 = 1;
+  for (i = 1; i < number; i++) {
+    num3 = num1 + num2;
+    num2 = num1;
+    num1 = num3;
+  }
+  return num3;
 };
-
+fibonacci("1");
 // Do not edit below this line
 module.exports = fibonacci;
