edba805ea280d202aa65c3708c3695ab307b1c2c
Titas-Lenkaitis <titas.lenkaitis1@gmail.com>
2023-04-21 21:13:06 +0100

    Complete exericse 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+  let sum = "";
+  let titles = arr.map(book => sum + book.title);
+  return titles;
 };
 
 // Do not edit below this line
