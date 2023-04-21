6619c6259a2a2ddaef9a1de9c949305942668bfb
Titas-Lenkaitis <titas.lenkaitis1@gmail.com>
2023-04-21 21:41:58 +0100

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  const orderedArr = arr.sort(function(a,b) {
+    if (typeof a.yearOfDeath == "undefined") {
+      a.yearOfDeath = new Date().getFullYear()
+    } else if (typeof b.yearOfDeath == "undefined") {
+      b.yearOfDeath = new Date().getFullYear()
+    }
+    if((a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth)) {
+      return -1;
+    } else {
+      return 1;
+    };
+  });
+  return orderedArr[0];
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
